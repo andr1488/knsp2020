1e50b9d3fa307a7245d886e7cad6fccf155268e1
Roman Vozniak <vozniakr98@gmail.com>
2020-06-15 19:40:21 +0300

    Vozniak: lab 4 remove redundant code.

--- Vozniak/Lab4/JavaClasses/GAEvolve.java
+++ Vozniak/Lab4/JavaClasses/GAEvolve.java
@@ -1,4 +1,4 @@
-public class GAEvolve {
+﻿public class GAEvolve {
 	public float[] best_fitness;// best_fitness[i] є найкращим значенням для
 								// і-го покоління даної еволюції
 	public GAIndividual best_ind; // найкращий індивідум(х) даної еволюції
@@ -17,18 +17,13 @@ public class GAEvolve {
 			gap = gap.generate(gap, xrate, mrate, min_range, max_range);
 			best_fitness[i] = gap.ind[gap.best_index].fitness;
 			// if(i<11)
-			System.out
-					.println("Найкраще значення покоління " + best_fitness[i]);
+			System.out.println("Найкраще значення покоління " + best_fitness[i]);
 		}
 		best_ind = gap.ind[gap.best_index];
 	}
 
 	public static void main(String[] args) {
-		// вибір критерію пошуку мінімуму або максимуму 
-		// для пошуку мінімуму
-//		float[] min = new float[] { 5f };
-//		float[] max = new float[] { 15f };
-		// для пошуку максимуму
+		// вибір критерію пошуку мінімуму або максимуму
 		float[] min = new float[] { -10f };
 		float[] max = new float[] { 10 };
 		GAEvolve gae = new GAEvolve(10, 20, 1, 80, 20, min, max);
